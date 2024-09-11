fb78e1a63622c59b3406012957857c39d6e0e935
katerina <katerina_panina1@mail.ru>
2024-09-11 11:45:31 +0300

    add repositories

--- src/main/java/ru/panina/springproject/repositories/BooksRepository.java
+++ src/main/java/ru/panina/springproject/repositories/BooksRepository.java
@@ -1,4 +1,9 @@
 package ru.panina.springproject.repositories;
 
-public interface BooksRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.panina.springproject.models.Book;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Book, Integer> {
 }
--- src/main/java/ru/panina/springproject/repositories/PeopleRepository.java
+++ src/main/java/ru/panina/springproject/repositories/PeopleRepository.java
@@ -1,4 +1,10 @@
 package ru.panina.springproject.repositories;
 
-public interface PeopleRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.panina.springproject.models.Person;
+
+@Repository
+public interface PeopleRepository extends JpaRepository<Person, Integer> {
+    public Person findByName(String name);
 }
